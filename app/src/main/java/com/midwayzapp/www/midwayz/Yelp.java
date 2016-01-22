094b68752b05612a3716885f54a1a0adf0b5f1df
Andrew Dyer <adyer1@luc.edu>
2016-01-22 16:22:38 -0600

    Yelp API Set UP

---
+++ app/src/main/java/com/midwayzapp/www/midwayz/Yelp.java
@@ -0,0 +1,59 @@
+package com.midwayzapp.www.midwayz;
+
+        import android.content.Context;
+        import org.scribe.builder.ServiceBuilder;
+        import org.scribe.model.OAuthRequest;
+        import org.scribe.model.Response;
+        import org.scribe.model.Token;
+        import org.scribe.model.Verb;
+        import org.scribe.oauth.OAuthService;
+
+/**
+ * Example for accessing the Yelp API.
+ */
+public class Yelp {
+
+    OAuthService service;
+    Token accessToken;
+
+    public static Yelp getYelp(Context context) {
+        return new Yelp(context.getString(R.string.consumer_key), context.getString(R.string.consumer_secret),
+                context.getString(R.string.token), context.getString(R.string.token_secret));
+    }
+
+    /**
+     * Setup the Yelp API OAuth credentials.
+     *
+     * OAuth credentials are available from the developer site, under Manage API access (version 2 API).
+     *
+     * @param consumerKey Consumer key
+     * @param consumerSecret Consumer secret
+     * @param token Token
+     * @param tokenSecret Token secret
+     */
+    public Yelp(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        this.service = new ServiceBuilder().provider(TwoStepOAuth.class).apiKey(consumerKey).apiSecret(consumerSecret).build();
+        this.accessToken = new Token(token, tokenSecret);
+    }
+
+    /**
+     * Search with term and location.
+     *
+     * @param term Search term
+     * @param latitude Latitude
+     * @param longitude Longitude
+     * @return JSON string response
+     */
+    public String search(String term, double latitude, double longitude) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.yelp.com/v2/search");
+        request.addQuerystringParameter("term", term);
+        request.addQuerystringParameter("ll", latitude + "," + longitude);
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+
+
+
+}
